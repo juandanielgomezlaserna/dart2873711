8910f9959ae62c8ca67ab2d5c1efbd82d52db749
juandaniel <juandanielnube@gmail.com>
2024-04-23 08:09:41 -0500

    hola

---
+++ ejemplosDart/condicionales01.dart
@@ -0,0 +1,36 @@
+void main(){
+  print("******************");
+  //CONDICIONAL SIMPLE
+  int edad;
+  edad = 19;
+  if(edad >=18){
+    print("Usted es mayor de edad");
+  }
+  print("******************")
+  //CONDICIONAL DOBLE
+  bool esMayor = true; //Definición y Asignación
+  if(esMayor == true){//SI
+    print("Es mayor de edad");
+  }else{//SINO
+    print("Es menor de edad");
+  }
+  print("CONDICIONAL DOBLE #2");
+  double nota = 4.5;
+  if (nota >= 3){
+    print("Usted aprobó el exámen");
+  } else{
+    print("Usted no aprobó el exámen");
+  }
+
+  //CONDICIONAL ANIDADO
+  int estrato = 2;
+  if (estrato == 1){
+    print("Usted es estrato 1");
+    print("Se le subsidia ek 10% de la factura");
+  }else if(estrato == 2){
+    print("Usted es estrato 2");
+    print("Se le subsidia el 5% de la factura");
+  }else if(estrato == 3){
+    
+  }
+  }
