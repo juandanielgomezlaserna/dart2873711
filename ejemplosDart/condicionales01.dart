362974050c07235f23a5e44bb39cc1c993e06644
juandanielgomezlaserna <159267786+juandanielgomezlaserna@users.noreply.github.com>
2024-04-23 08:02:16 -0500

    Update condicionales01.dart
    condicionales01

---
+++ ejemplosDart/condicionales01.dart
@@ -0,0 +1,24 @@
+void main(){
+  print("******************");
+  //CONDICIONAL SIMPLE
+  int edad;
+  edad = 19;
+  if(edad >=18){
+    print("Usted es mayor de edad");
+  }
+  print("******************")
+  //CONDICIONAL DOBLE
+  bool esMayor = true; //Definición y Asignación
+  if(esMayor == true){//SI
+    print("Es mayor de edad");
+  }else{//SINO
+    print("Es menor de edad");
+  }
+  print("CONDICIONAL DOBLE #2");
+  double nota = 4.5;
+  if (nota >= 3){
+    print("Usted aprobó el exámen");
+  } else{
+    print("Usted no aprobó el exámen");
+  }
+  }
