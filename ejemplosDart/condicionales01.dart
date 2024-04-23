289f364feca76cb677d744448e393e4ad16cc91a
juandaniel <juandanielnube@gmail.com>
2024-04-23 08:18:46 -0500

    Se agregaron cambios

--- ejemplosDart/condicionales01.dart
+++ ejemplosDart/condicionales01.dart
@@ -6,7 +6,7 @@ void main(){
   if(edad >=18){
     print("Usted es mayor de edad");
   }
-  print("******************")
+  print("******************");
   //CONDICIONAL DOBLE
   bool esMayor = true; //Definición y Asignación
   if(esMayor == true){//SI
@@ -31,6 +31,18 @@ void main(){
     print("Usted es estrato 2");
     print("Se le subsidia el 5% de la factura");
   }else if(estrato == 3){
-    
+    print("Usted es estrato 3");
+    print("Se le subsidia el 3% de la factura");
+  }else if(estrato == 4){
+    print("Usted es estrato 4");
+    print("Paga un 5% más de la factura");
+  }else if(estrato == 5){
+    print("Usted es estrato 5");
+    print("paga un 10% más de la factura");
+  }else if(estrato == 6){
+    print("Usted es estrato 6");
+    print("paga un 15% más de la factura");
+  }else{
+    print("El estrato es incorrecto");
   }
   }
