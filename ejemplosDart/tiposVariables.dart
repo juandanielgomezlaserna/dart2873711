25b6e463a8ac73dc6d83dfff7eaf6bceb28e6b48
juandaniel <juandanielnube@gmail.com>
2024-04-23 07:56:57 -0500

    Se creó un repositorio

---
+++ ejemplosDart/tiposVariables.dart
@@ -0,0 +1,20 @@
+void main() {
+  /*
+  TIPOS DE VARIABLES
+  STRING: Tipo cadena
+  int: tipo número entero
+  double: numero real
+  bool: tipo booleano
+
+  */
+  String nombre;
+  int edad;
+  double altura; //es el float
+  bool estado;
+  nombre = "Juan";
+  edad = 18;
+  altura = 1.70;
+  estado = true;
+  print("Su nombre es $nombre");
+  print("Su altura es $altura y su edad es $edad");
+}
---
+++ ejemplosDart/tiposVariables02.dart
@@ -0,0 +1,29 @@
+void main(){
+  print("Tipos de variables");
+  print("Tipo ENTERO");
+  int cantExamenes; //Definición variable
+  cantExamenes = 3; //Asignacion Variable
+  int cantEstudiantes = 30; //Definición/Asignación vble
+  print("La cantidad de estudiantes es $cantEstudiantes");
+  print("Deben presentar $cantExamenes examenes");
+  print("**********************");
+  print("Tipo REAL");
+  double peso; //Definición
+  peso = 70.5; //Asignación
+  double promedio = 3.5; //Definición y asignación
+  print("Su peso es $peso y su promedio es: $promedio");
+  print("***************************");
+  print("TIPO CADENA");
+  String telefono;
+  telefono = "3137019506";
+  String email = "juandaniel.1gmail.com";
+  print("Su telefono es $telefono y su email es $email");
+  print("***********************");
+  print("TIPO BOOLEANO");
+  bool esCasado;
+  esCasado = false;
+  bool tieneHijos = true;
+  print("Es casado: $esCasado");
+  print("Tiene hijos: $tieneHijos")
+
+}
